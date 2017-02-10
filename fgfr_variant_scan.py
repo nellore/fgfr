85040685ee98dfbd3c231e7cf1fc92746234de0d
Abhi Nellore <anellore@gmail.com>
2017-02-09 17:13:18 -0800

    uses py-bigwig

--- fgfr_variant_scan.py
+++ fgfr_variant_scan.py
@@ -10,24 +10,25 @@ bigwigs.
 
 Output: TSV with file uuids and raw coverages of G>A at 
 chr5:177093734-177093735 iff any G>A reads are detected.
+
+Requires https://github.com/brentp/bw-python
 """
-import pyBigWig
+from bw import BigWig
 import sys
 import os
 
 if __name__ == '__main__':
     root_dir = sys.argv[1]
     for batch_dir in glob.glob(os.path.join(root_dir, 'batch_*')):
-        for bw in glob.glob(os.path.join(batch_dir, '*.G.bw')):
-            if bw.count('.') < blah:
-                '''Conditional excludes all files with "unique" in filenam, 
-                which correspond to bigwigs considering unique alignments only.
-                We use the bigwigs that count all primary alignments.'''
-                bw_stream = pyBigWig.open(bw)
-                try:
-                   coverage = bw_stream.values('chr5', 177093734, 177093735)[0]
-                except IndexError:
-                    # Nothing to see in this bigwig
-                    pass
-                else:
-                    print '\t'.join([bw, str(coverage)])
+        for bigwig in glob.glob(os.path.join(batch_dir, '*.G.bw')):
+            '''Excludes all files with "unique" in filenam, 
+            which correspond to bigwigs considering unique alignments only.
+            We use the bigwigs that count all primary alignments.'''
+            bw_stream = pyBigWig.open(bw)
+            try:
+               coverage = bw_stream.values('chr5', 177093734, 177093735)[0]
+            except IndexError:
+                # Nothing to see in this bigwig
+                pass
+            else:
+                print '\t'.join([bw, str(coverage)])
